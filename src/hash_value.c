0b26d93a6d2baed8f4a0eddcc6539b7bf99c32d8
Linkz <39253822+Linkz64@users.noreply.github.com>
2025-06-28 10:54:34 +0100

    Corrected AIrandom .bss end

--- src/hash_value.c
+++ src/hash_value.c
@@ -1,6 +1,10 @@
 /*
+0x801ccf40 _AIrand
+0x801ccf68 BXrand
+0x801ccf90 BXsrand
 0x801ccfbc GetHashValue32
 0x801ccfe4 tHashName32::getHashValue
+//end 0x801CD02C
 
 */
 
@@ -8,11 +12,9 @@ typedef unsigned int uint;
 
 uint GetHashValue32(char* str);
 uint tHashName32_scoperes_getHashValue(uint*, char*);
-//extern void cRandom_scoperes_seed(uint *param_1, uint param_2);
 
-//extern uint* BXrandom; 
-char AIrandom[24]; // 24 bytes long
-char BXrandom[24]; // 24 bytes long
+char AIrandom[24];
+char BXrandom[24];
 
 
 uint _AIrand(void){
@@ -33,19 +35,23 @@ uint GetHashValue32(char* str){
     return tHashName32_scoperes_getHashValue((uint*)hash, str);
 }
 
+
 uint tHashName32_scoperes_getHashValue(uint* out, char* str){
     uint hash = 0;
-    uint top = 0;
+    uint top;
+    uint shifted;
 
     while (*str) {
-        hash = (hash << 4) + (signed char)(*str++);
+        hash = (hash << 4) + *str++;
         top = hash & 0xF0000000;
+
         if (top != 0) {
-            hash ^= (top >> 23);
+            shifted = top >> 23;
+            hash ^= shifted;
             hash ^= top;
         }
     }
 
     *out = hash;
-    return hash;
+    return *out;
 }
