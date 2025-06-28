63fda54ac2a9aa00eaaa97f3cb911966ecf0a625
Linkz <39253822+Linkz64@users.noreply.github.com>
2025-06-28 06:35:58 +0100

    tHashName32::getHashValue

--- src/hash_value.c
+++ src/hash_value.c
@@ -1,8 +1,30 @@
-void GetHashValue32(void * param_1);
+/*
+0x801ccfbc GetHashValue32
+0x801ccfe4 tHashName32::getHashValue
+
+*/
+int GetHashValue32(char* param_1);
+int tHashName32_scoperes_getHashValue(int*, char*);
 
 
-void GetHashValue32(void* param_1){
-    char local [8];
-    fn_801CCFE4(local, param_1);
-    return;
+int GetHashValue32(char* param_1){
+    char hash[4];
+    return tHashName32_scoperes_getHashValue((int*)hash, param_1);
+}
+
+int tHashName32_scoperes_getHashValue(int* out, char* str){
+    int hash = 0;
+    int top = 0;
+
+    while (*str) {
+        hash = (hash << 4) + (signed char)(*str++);
+        top = hash & 0xF0000000;
+        if (top != 0) {
+            hash ^= (top >> 23);
+            hash ^= top;
+        }
+    }
+
+    *out = hash;
+    return hash;
 }
