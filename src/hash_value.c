7bd281d61a4f14177aa8f4c5c49cbcd601756212
Linkz <39253822+Linkz64@users.noreply.github.com>
2025-06-28 08:45:45 +0100

    BXrand

--- src/hash_value.c
+++ src/hash_value.c
@@ -8,23 +8,17 @@ typedef unsigned int uint;
 
 uint GetHashValue32(char* str);
 uint tHashName32_scoperes_getHashValue(uint*, char*);
-//void cRandom_scoperes_seed(uint param_1, uint param_2);
+//extern void cRandom_scoperes_seed(uint *param_1, uint param_2);
 
-//uint BXrandom;
 extern uint* BXrandom;
 
+void BXrand(void){
+    cRandom_scoperes_random(&BXrandom);
+}
 
 void BXsrand(uint param_1){
     cRandom_scoperes_seed(BXrandom, param_1);
 }
-
-// extern void* fn_random;                    // declared elsewhere
-// extern void lbl_random_seed(void*, void*); // takes a function pointer and argument
-
-// void wrapper_call_random_seed(void* arg) {
-//     fun_random_seed(&dat_random, arg);
-// }
-
 
 
 uint GetHashValue32(char* str){
